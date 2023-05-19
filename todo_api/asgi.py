a1880cc3469256ae722db4a370b16c6fc9bec7da
Mahmudul Alam <mahmudula2000@gmail.com>
2023-05-20 04:31:53 +0600

    updated environment settings to be setted first
    updated environment settings to be setted first

--- todo_api/asgi.py
+++ todo_api/asgi.py
@@ -3,11 +3,11 @@ import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todo_api.settings")
+
 import core.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todo_api.settings")
 django_asgi_app = get_asgi_application()
-
 
 application = ProtocolTypeRouter(
     {
